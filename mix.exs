9fa691959d4b2f8e03b3ca2123152ee2c5496de7
yuki-toida <yuki.toidax@gmail.com>
2018-06-25 20:10:34 +0900

    add version

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule ExPayjp.MixProject do
   def project do
     [
       app: :ex_payjp,
-      version: "0.3.0",
+      version: "0.4.0",
       elixir: "~> 1.6",
       name: "ExPayjp",
       description: @description,
